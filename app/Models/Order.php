17ec1592d4161d51177a8c4cb91058170977627d
prawpash <robbyprawira3@gmail.com>
2026-08-03 22:49:24 +0700

    feat: Order Factory
    Create order factory to simplify the order creation process

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
+    use HasFactory;
+
     /**
      * Get the user that owns the order.
      */
